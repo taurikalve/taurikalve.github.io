18650cb42193336e251102bd03e6b40495d312a8
Tauri Kalve <tauri@digiway.ee>
2025-05-16 12:56:04 +0300

    intro improvement

--- src/components/content/intro/Intro.tsx
+++ src/components/content/intro/Intro.tsx
@@ -11,16 +11,15 @@ export default function Intro({
     <div className={buildClasses(style.container, className)} {...rest}>
       <p>
         I'm a Full-Stack Web Developer with 12 years of experience. I have
-        extensive experience creating microservices architecture apps and can
-        handle everything from planning the project to writing front-end.
+        extensive experience creating and microservices architecture web
+        applications. I specialize in leading projects end-to-end from inital
+        planning to front-end development and deployment.
       </p>
       <p>
-        I like to drive for excellence and I'm constantly looking for new ways
-        to improve both my productivity and the quality of my output.
-      </p>
-      <p>
-        I'm looking for a job, where I can focus on what I love doing most -
-        delivering products that exceed expectations.
+        I'm passionate about delivering high-quality products and constantly
+        refining my skills to improve both productivity and code quality. I'm
+        now seeking a role where I can focus on what I love doing most -
+        creating solutions that exceed expectations.
       </p>
     </div>
   );
