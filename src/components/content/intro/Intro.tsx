0c70b291da42248217b32d50976c29d4b4d84b58
Tauri Kalve <tauri@digiway.ee>
2025-05-17 15:30:01 +0300

    content improvement

--- src/components/content/intro/Intro.tsx
+++ src/components/content/intro/Intro.tsx
@@ -10,16 +10,21 @@ export default function Intro({
   return (
     <div className={buildClasses(style.container, className)} {...rest}>
       <p>
-        I'm a Full-Stack Web Developer with 12 years of experience. I have
-        extensive experience creating and managing microservices architecture
-        web applications. I specialize in leading projects end-to-end from
-        inital planning to front-end development and deployment.
+        I'm a Full-Stack Developer with 12 years of experience building dynamic,
+        user-focused web applications. I specialize in crafting responsive
+        front-end interfaces with React and TypeScript, while also designing
+        scalable back-end systems using Node.js, REST/GraphQL and MongoDB/SQL.
+        My expertise includes developing microservices architectures and
+        optimizing end-to-end workflows, from pixel-perfect UIs to robust APIs
+        and CI/CD pipelines.
       </p>
       <p>
-        I'm passionate about delivering high-quality products and constantly
-        refining my skills to improve both productivity and code quality. I'm
-        now seeking a role where I can focus on what I love doing most -
-        creating solutions that exceed expectations.
+        I'm passionate about writing clean, efficient code and delivering
+        high-quality solutions that delight users and scale seamlessly. Whether
+        it's enhancing front-end performance or architecting full-stack systems,
+        I thrive on solving complex challenges and continuously refining my
+        skills. I'm seeking Front-End or Full-Stack Developer roles where I can
+        leverage my expertise to build innovative and impactful web experiences.
       </p>
     </div>
   );
